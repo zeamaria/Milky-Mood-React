f7e3cbaea6c422a107bda87b8d716929f5a55f8e
Zea <78378462+zeamaria@users.noreply.github.com>
2021-09-16 11:15:24 -0700

    initial commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FooterComponent = () => (
+  <div>
+    <h2>June 16</h2>
+    <h2>World Tour</h2>
+    <h2>Aug 16</h2>
+  </div>
+);
+
+export default FooterComponent;
